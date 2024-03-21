63f7341f7c3c3a1d16fd6eb18d390821734428c7
Abdulaziz Abdi <amigf76@gmail.com>
2024-03-21 17:52:51 +0300

    Function expressions and scope

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(saturdayActivity = 'roller-skate'){
+    return `This Saturday, I want to ${saturdayActivity}!`;
+}
+
+const mondayWork = function (mondayActivity = "go to the office"){
+    return `This Monday, I will ${mondayActivity}.`;
+}
+
+function wrapAdjective(visualParameter = "*"){
+    return function(adjectiveParameter = "special"){
+        return `You are ${visualParameter}${adjectiveParameter}${visualParameter}!`
+    };
+}
